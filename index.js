9082f1cbbc95c4393365fabee9af7b8cf6099e6f
Mooaz Sayyed <127180390+moooazsayyed@users.noreply.github.com>
2024-02-01 19:55:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+//index.js
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`)
+})
