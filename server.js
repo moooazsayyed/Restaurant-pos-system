c4e7948ebdc666e9efa936278988f44051db8998
mooaz sayyed <sayyedmooaz@gmail.com>
2024-02-04 21:02:46 +0530

    made server js to handle form to add products

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));//to serve our public folder throuhgh html
+
+
+//APi middleware
+app.use(express.json());//this is to accept data in json format
+app.use(express.urlencoded());// this is to decode data to send through html form
+let products = [];
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'products.html'));
+});
+
+app.post('/add-product', (req, res) => {
+    console.log(req.body);//the data we get is in the body of request
+    const productName = req.body.productName;
+    const productPrice = req.body.productPrice;
+     res.sendFile(path.join(__dirname, 'public', 'products.html'), {
+        productName,
+        productPrice,
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
