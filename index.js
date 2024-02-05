b47590d6c1c50952a10cb01edb7e83a468b1b1e3
mooaz sayyed <sayyedmooaz@gmail.com>
2024-02-05 21:06:53 +0530

    installed url module

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 //index.js
-const express = require('express')
-const app = express()
-const port = 3000
+const http = require("http");
+const fs = require("fs");
 
-app.use(express.static('public'));
+const myserver = http.createServer((req, resp) => {
+    const log = `${Date.now()} - New Request Received: ${req.url}`;
+    fs.appendFile('log.txt', log, (err, data) => {
+        if (err) {
+            console.error(err);
+        }
+    });
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`)
-})
+    switch (req.url) {
+        case "/":
+            resp.end("HomePage");
+            break;
+        case "/about":
+            resp.end("About Page");
+            break;
+        default:
+            resp.end("404 Not Found");
+    }
+});
+
+myserver.listen(3000, () => console.log("Server Started"));
